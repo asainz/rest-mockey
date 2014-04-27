48bd57d81193fd893e05d6c7646e987ccac0889c
Andres Sainz de Aja <andres.sainz@globant.com>
2014-04-27 16:45:23 -0300

    add basic routing for /api/

--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,18 @@ module.exports = function(app){
     var home = require('../app/controllers/home');
 
     app.get('/', home.index);
+
+    /*
+    localhost:3000/api/get-colors
+     */
+    
+    app.get(/^\/api(.*)$/, function(req, res){
+        var method = req.method;
+        var path = req.params[0];
+        var query = req.query;
+
+        var data = require('../app/data/profile.json');
+
+        res.json(data);
+    });
 };

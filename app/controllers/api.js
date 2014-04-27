390dffb55076ffa7045bc9cb597ccbc2423f9c92
Andres Sainz de Aja <andres.sainz@globant.com>
2014-04-27 17:06:01 -0300

    add services mock data and logic to return the correct api

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,9 +1,42 @@
 exports.index = function(req, res){
-    var method = req.method;
+    var method = req.method.toLowerCase();
     var path = req.params[0];
     var query = req.query;
 
-    var data = require('../data/profile.json');
+    /*
+    1. Validate request headers
+    2. Get service information
+    3. Get active scenario
+    4. Return it (CORS enabled)
+     */
 
-    res.json(data);
+    var data = require('../data/data.json');
+    var services = data.services;
+    var APIResponse;
+
+    services.forEach(function(service){
+        var responses;
+        if( service.path === path ){
+            responses = service.responses[method];
+            if( !responses ){
+                APIResponse = {
+                    error: "No scenario has been defined for this method."
+                };
+            }
+
+            responses.forEach(function(response){
+                if( response.active ){
+                    APIResponse = JSON.parse(response.body);
+                }
+            });
+
+            if( !APIResponse ){
+                APIResponse = {
+                    error: "There isn't any active scenario for this service."
+                };
+            }
+        }
+    });
+
+    res.json(APIResponse);
 };
